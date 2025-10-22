033ad45125ac436dafdefaac57dec069d86df38b
Roronoa_Maddy <madhavashanmugam28@gmail.com>
2025-10-22 11:28:18 +0530

    ChromeOptions Added for Downloading

--- ReqnrollProject1/ReqnrollProject1/Helpers/Browser_Extension.cs
+++ ReqnrollProject1/ReqnrollProject1/Helpers/Browser_Extension.cs
@@ -12,10 +12,27 @@ namespace ReqnrollProject1.Helpers
     {
         public static void InitiateChromeBrowser()
         {
-           Properties_Collection.Driver = new ChromeDriver();
-           Properties_Collection.Driver.Manage().Window.Maximize();
-           //Properties_Collection.Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
-            
+            //string downloadPath = Path.Combine(Directory.GetCurrentDirectory(), "Downloads");
+            //Properties_Collection.Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+            ChromeOptions options = new ChromeOptions();
+            string projectRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\.."));
+            string downloadPath = Path.Combine(projectRoot, "Automation_Downloads");
+            // string downloadPath = Path.Combine(Directory.GetCurrentDirectory(), "Automation_Downloads");
+            //Directory.CreateDirectory(downloadPath); // Create folder if it doesn't exist
+
+            // ⭐ SET ALL PREFERENCES FIRST (BEFORE creating driver)
+            options.AddUserProfilePreference("download.default_directory", downloadPath);
+            options.AddUserProfilePreference("download.prompt_for_download", false);
+            options.AddUserProfilePreference("download.directory_upgrade", true);
+            options.AddUserProfilePreference("safebrowsing.enabled", false);
+            options.AddUserProfilePreference("safebrowsing.disable_download_protection", true);
+            options.AddUserProfilePreference("profile.default_content_settings.popups", 0);
+            options.AddUserProfilePreference("profile.default_content_setting_values.automatic_downloads", 1);
+
+            // ⭐ CREATE DRIVER LAST (AFTER all preferences are set)
+            Properties_Collection.Driver = new ChromeDriver(options);
+            Properties_Collection.Driver.Manage().Window.Maximize();
+
         }
 
 
