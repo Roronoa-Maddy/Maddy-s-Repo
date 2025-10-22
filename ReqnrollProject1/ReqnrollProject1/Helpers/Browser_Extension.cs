cf19c9ede136314451d852ed3967ecaca5e95e5a
Roronoa_Maddy <madhavashanmugam28@gmail.com>
2025-10-22 11:34:42 +0530

    committe changes

--- ReqnrollProject1/ReqnrollProject1/Helpers/Browser_Extension.cs
+++ ReqnrollProject1/ReqnrollProject1/Helpers/Browser_Extension.cs
@@ -33,6 +33,10 @@ namespace ReqnrollProject1.Helpers
             Properties_Collection.Driver = new ChromeDriver(options);
             Properties_Collection.Driver.Manage().Window.Maximize();
 
+
+            //I am going to add a new line here 
+            // Going to push new code there
+
         }
 
 
